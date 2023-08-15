94426f5d17428934184ae9809a97ded10256385c
Tim Perdue <tp.webdev@gmail.com>
2023-08-14 22:18:58 -0400

    Correcting path to autload.php file

--- public/index.php
+++ public/index.php
@@ -1,5 +1,13 @@
 <?php
 
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
 require_once __DIR__ . '/../bootstrap/app.php';
+
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write('Hello World');
+    return $response;
+});
 
 $app->run();

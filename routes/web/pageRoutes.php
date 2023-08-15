b3afc25ed83bafa724ef939a989d9172212e067e
Tim Perdue <tp.webdev@gmail.com>
2023-08-14 23:03:51 -0400

    Refactoring pageRoutes to use PageController

--- routes/web/pageRoutes.php
+++ routes/web/pageRoutes.php
@@ -1,10 +1,7 @@
 <?php
 
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Psr\Http\Message\ResponseInterface as Response;
+use App\Controllers\PageController;
 
 
-$app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write('Hello World');
-    return $response;
-});
+
+$app->get('/', PageController::class . ':index')->setName('home');

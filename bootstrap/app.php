21707af36a7101ec43e5670d01755e95b8cb0ab3
Tim Perdue <tp.webdev@gmail.com>
2023-08-14 22:39:18 -0400

    Refactoring routing structure

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,4 +4,9 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
 
+# Create App
 $app = AppFactory::create();
+
+# Load Routes
+require_once __DIR__ . '/../routes/web/pageRoutes.php';
+require_once __DIR__ . '/../routes/api/adminApiRoutes.php';
---
+++ routes/web/pageRoutes.php
@@ -0,0 +1,10 @@
+<?php
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write('Hello World');
+    return $response;
+});

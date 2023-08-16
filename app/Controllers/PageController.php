1276146bb99400cf9f39ada1cbf80dbb0110e59a
Tim Perdue <tp.webdev@gmail.com>
2023-08-16 00:23:14 -0400

    Adding var-dump package

--- app/Controllers/PageController.php
+++ app/Controllers/PageController.php
@@ -9,6 +9,7 @@ class PageController extends BaseController
 {
     public function index(Request $request, Response $response)
     {
+
         $appName = $this->container->get('settings')['app']['name'];
         return $this->container->get('view')->render($response, 'home.twig', ['appName' => $appName]);
     }
